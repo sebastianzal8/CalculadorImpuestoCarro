a56874917d74a490acf67bfdfd717ece6e75b7d1
Ricardo Aldana <raldana67450@universidadean.edu.co>
2021-08-20 22:46:43 -0500

    Se realiza consulta de precio, pero falta por analizar

--- src/universidadean/impuestoscarro/mundo/CalculadorImpuestos.java
+++ src/universidadean/impuestoscarro/mundo/CalculadorImpuestos.java
@@ -293,13 +293,28 @@ public class CalculadorImpuestos {
      * @return El vehículo más caro.
      */
     public Vehiculo buscarVehiculoMasCaro() {
+       /** double precioVeh = ...;
+        String nombreVeh = ...);
+
+        for (Vehiculo v : vehiculos ){
+            if (v.darPrecio() > precioVeh){
+                precioVeh = v.darPrecio();
+                nombreVeh = v.darMarca();
+            }
+        }
+        return nombreVeh;*/
+        Vehiculo mascaro;
+
+
+
         Vehiculo masCaro = null;
 
         // TODO: Buscar el vehículo más caro del arreglo de vehículos
 
         return masCaro;
-
     }
+
+
 
     /**
      * Busca y retorna el primer vehículo que encuentra con la marca que llega por parámetro. <br>
